4d40109215129ae2c7f509ca0022202edb90be0d
alexandru.bencheci <alexandru.bencheci@gmail.com>
2025-01-23 23:34:50 +0200

    add root schema

--- src/routes/route.ts
+++ src/routes/route.ts
@@ -1,7 +1,21 @@
 import type { FastifyInstance } from 'fastify';
 
 export default async function (app: FastifyInstance) {
-  app.get('/', (request, reply) => {
+  app.get('/', { schema }, (request, reply) => {
     reply.send({ info: process.env.API_TITLE });
   });
 }
+
+const schema = {
+  tags: ['System'],
+  summary: 'Get API info',
+  response: {
+    200: {
+      description: 'OK',
+      type: 'object',
+      properties: {
+        info: { type: 'string' }
+      }
+    }
+  }
+};
